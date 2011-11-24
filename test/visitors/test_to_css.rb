b4daa24885e872f1ce743c689b17381e8a14e171
Chad Weider <cweider@oofn.net>
2011-11-24 12:32:41 -0800

    Template idempotent assertion.

--- test/visitors/test_to_css.rb
+++ test/visitors/test_to_css.rb
@@ -3,13 +3,24 @@ require 'helper'
 module CSSPool
   module Visitors
     class TestToCSS < CSSPool::TestCase
+      def assert_idempotent doc_or_source
+        doc = if doc_or_source.is_a? CSSPool::CSS::Document
+          doc_or_source
+        else
+          CSSPool.CSS doc_or_source.to_s
+        end
+        yield doc
+        yield CSSPool.CSS doc.to_css
+
+        doc_or_source
+      end
+
       def test_font_serialization
         doc = CSSPool.CSS 'p { font: 14px/30px Tahoma; }'
 
-        assert_equal 3, doc.rule_sets.first.declarations[0].expressions.length
-
-        doc = CSSPool.CSS(doc.to_css)
-        assert_equal 3, doc.rule_sets.first.declarations[0].expressions.length
+        assert_idempotent doc do |doc|
+          assert_equal 3, doc.rule_sets.first.declarations[0].expressions.length
+        end
 
         assert_equal 'font: 14px / 30px Tahoma;',
           doc.rule_sets.first.declarations.first.to_css.strip
@@ -29,83 +40,69 @@ module CSSPool
       #end
 
       def test_hash_operator
-        doc = CSSPool.CSS 'p { font: foo, #666; }'
-        assert_equal 2, doc.rule_sets.first.declarations[0].expressions.length
-
-        doc = CSSPool.CSS(doc.to_css)
-        assert_equal 2, doc.rule_sets.first.declarations[0].expressions.length
+        assert_idempotent 'p { font: foo, #666; }' do |doc|
+          assert_equal 2, doc.rule_sets.first.declarations[0].expressions.length
+        end
       end
 
       def test_uri_operator
-        doc = CSSPool.CSS 'p { font: foo, url(http://example.com/); }'
-        assert_equal 2, doc.rule_sets.first.declarations[0].expressions.length
-
-        doc = CSSPool.CSS(doc.to_css)
-        assert_equal 2, doc.rule_sets.first.declarations[0].expressions.length
+        assert_idempotent 'p { font: foo, url(http://example.com/); }' do |doc|
+          assert_equal 2, doc.rule_sets.first.declarations[0].expressions.length
+        end
       end
 
       def test_string_operator
-        doc = CSSPool.CSS 'p { font: foo, "foo"; }'
-        assert_equal 2, doc.rule_sets.first.declarations[0].expressions.length
-
-        doc = CSSPool.CSS(doc.to_css)
-        assert_equal 2, doc.rule_sets.first.declarations[0].expressions.length
+        assert_idempotent 'p { font: foo, "foo"; }' do |doc|
+          assert_equal 2, doc.rule_sets.first.declarations[0].expressions.length
+        end
       end
 
       def test_function_operator
-        doc = CSSPool.CSS 'p { font: foo, foo(1); }'
-        assert_equal 2, doc.rule_sets.first.declarations[0].expressions.length
-
-        doc = CSSPool.CSS(doc.to_css)
-        assert_equal 2, doc.rule_sets.first.declarations[0].expressions.length
+        assert_idempotent 'p { font: foo, foo(1); }' do |doc|
+          assert_equal 2, doc.rule_sets.first.declarations[0].expressions.length
+        end
       end
 
       def test_rgb_operator
-        doc = CSSPool.CSS 'p { font: foo, rgb(1,2,3); }'
-        assert_equal 2, doc.rule_sets.first.declarations[0].expressions.length
-
-        doc = CSSPool.CSS(doc.to_css)
-        assert_equal 2, doc.rule_sets.first.declarations[0].expressions.length
+        assert_idempotent 'p { font: foo, rgb(1,2,3); }' do |doc|
+          assert_equal 2, doc.rule_sets.first.declarations[0].expressions.length
+        end
       end
 
       def test_includes
-        doc = CSSPool.CSS <<-eocss
+        source = <<-eocss
           div[bar ~= 'adsf'] { background: red, blue; }
         eocss
 
-        assert_equal 1, doc.rule_sets.first.selectors.first.simple_selectors.first.additional_selectors.length
-        assert_equal 2, doc.rule_sets.first.declarations[0].expressions.length
-
-        doc = CSSPool.CSS(doc.to_css)
-        assert_equal 1, doc.rule_sets.first.selectors.first.simple_selectors.first.additional_selectors.length
-        assert_equal 2, doc.rule_sets.first.declarations[0].expressions.length
+        assert_idempotent source do |doc|
+          assert_equal 1, doc.rule_sets.first.selectors.first.simple_selectors.first.additional_selectors.length
+          assert_equal 2, doc.rule_sets.first.declarations[0].expressions.length
+        end
       end
 
       def test_dashmatch
-        doc = CSSPool.CSS <<-eocss
+        source = <<-eocss
           div[bar |= 'adsf'] { background: red, blue; }
         eocss
 
-        assert_equal 1, doc.rule_sets.first.selectors.first.simple_selectors.first.additional_selectors.length
-
-        doc = CSSPool.CSS(doc.to_css)
-        assert_equal 1, doc.rule_sets.first.selectors.first.simple_selectors.first.additional_selectors.length
+        assert_idempotent source do |doc|
+          assert_equal 1, doc.rule_sets.first.selectors.first.simple_selectors.first.additional_selectors.length
+        end
       end
 
       def test_media
-        doc = CSSPool.CSS <<-eocss
+        source = <<-eocss
           @media print {
             div { background: red, blue; }
           }
         eocss
-        assert_equal 1, doc.rule_sets.first.media.length
-
-        doc = CSSPool.CSS(doc.to_css)
-        assert_equal 1, doc.rule_sets.first.media.length
+        assert_idempotent source do |doc|
+          assert_equal 1, doc.rule_sets.first.media.length
+        end
       end
 
       def test_multiple_media
-        doc = CSSPool.CSS <<-eocss
+        source = <<-eocss
           @media print, screen {
             div { background: red, blue; }
           }
@@ -114,40 +111,33 @@ module CSSPool
             div { background: red, blue; }
           }
         eocss
-        assert_equal 2, doc.rule_sets.first.media.length
-        assert_equal 1, doc.rule_sets[1].media.length
-
-        doc = CSSPool.CSS(doc.to_css)
-        assert_equal 2, doc.rule_sets.first.media.length
-        assert_equal 1, doc.rule_sets[1].media.length
+        assert_idempotent source do |doc|
+          assert_equal 2, doc.rule_sets.first.media.length
+          assert_equal 1, doc.rule_sets[1].media.length
+        end
       end
 
       def test_import
-        doc = CSSPool.CSS <<-eocss
+        source = <<-eocss
           @import "test.css";
           @import url("test.css");
           @import url("test.css") print, screen;
         eocss
 
-        assert_equal 3, doc.import_rules.length
-        assert_equal 2, doc.import_rules.last.media.length
-
-        doc = CSSPool.CSS(doc.to_css)
-
-        assert_equal 3, doc.import_rules.length
-        assert_equal 2, doc.import_rules.last.media.length
+        assert_idempotent source do |doc|
+          assert_equal 3, doc.import_rules.length
+          assert_equal 2, doc.import_rules.last.media.length
+        end
       end
 
       def test_charsets
-        doc = CSSPool.CSS <<-eocss
+        source = <<-eocss
           @charset "UTF-8";
         eocss
 
-        assert_equal 1, doc.charsets.length
-
-        doc = CSSPool.CSS(doc.to_css)
-
-        assert_equal 1, doc.charsets.length
+        assert_idempotent source do |doc|
+          assert_equal 1, doc.charsets.length
+        end
       end
     end
   end
